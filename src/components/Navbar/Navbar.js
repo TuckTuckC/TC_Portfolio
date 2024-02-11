903dcceb8eac2ad8e7b78cf133bd80d3993bf5cd
Tucker Craig <tuckermcraig@gmail.com>
2024-02-11 14:59:23 -0500

    fixed navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-scroll'
 
 function Navbar({executeScroll, aboutRef, workRef, expRef}) {
   return (
-    <div className='bg-gray-500 p-3 h-16 flex flex-row items-center justify-evenly fixed inset-x-0 bottom-0 md:bottom-auto md:hidden'>
+    <div className='bg-gray-500 p-3 h-16 flex flex-row items-center justify-evenly fixed inset-x-0 bottom-0 md:hidden'>
         <img src={TC} className="w-16 p-3 text-white"></img>
         <div onClick={() => executeScroll(aboutRef)}>About</div>
         <div onClick={() => executeScroll(workRef)}>My Work</div>
